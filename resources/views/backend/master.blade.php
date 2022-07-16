c8a906502e4ce435b9f19ce51091014c5613c794
TareqImam <tareqimam800@gmail.com>
2022-07-16 13:38:43 +0600

    blading updated

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="{{url('backend/vendors/typicons/typicons.css')}}">
     <link rel="stylesheet" href="{{url('backend/vendors/simple-line-icons/css/simple-line-icons.css')}}">
     <link rel="stylesheet" href="{{url('backend/vendors/css/vendor.bundle.base.css')}}">
-    <link rel="stylesheet" href="{{url('backend/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
+    <link rel="stylesheet" href="{{url('backend/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}">
     <link rel="stylesheet" href="{{url('backend/js/select.dataTables.min.css')}}">
     <link rel="stylesheet" href="{{url('backend/css/vertical-layout-light/style.css')}}">
     <link rel="shortcut icon" href="{{url('backend/images/logo.svg')}}" />
