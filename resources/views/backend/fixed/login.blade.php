6742655d6f5f6e232054a0b9f264c9db4199ae0b
TareqImam <tareqimam800@gmail.com>
2022-08-14 21:38:31 +0600

    admin panel><

--- resources/views/backend/fixed/login.blade.php
+++ resources/views/backend/fixed/login.blade.php
@@ -21,7 +21,7 @@
 
             <!-- Icon -->
             <div class="fadeIn first">
-                <h3 class="m-3" style="color:#FB246A;">Admin Login</h3>
+                <h3 class="m-3" style="color:#2E59D9;">Admin Login</h3>
             </div>
 
             <!-- Login Form -->
@@ -29,7 +29,7 @@
                 @csrf
                 <input type="text" id="login" class="fadeIn second" name="email" placeholder="email">
                 <input type="password" id="password" class="fadeIn third" name="password" placeholder="password">
-                <input type="submit" class="fadeIn fourth" value="Log In">
+                <input type="submit" style="background-color:#2E59D9;" class="fadeIn fourth" value="Log In">
             </form>
 
         </div>
